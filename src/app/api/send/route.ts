e20734d07968eb8cbfaa2544cde6a3425651bc0e
David Asaolu <67129211+dha-stix@users.noreply.github.com>
2024-04-14 08:04:51 +0100

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,18 +1,14 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Novu } from "@novu/node";
-import { Echo } from "@novu/echo";
 import { SubscribersData } from "../../util";
+import { NextRequest, NextResponse } from "next/server";
+import { SubscribersData } from "../../util";
+import { Novu } from "@novu/node";
+
+//👇🏻 Novu SDK for sending emails
 const novu = new Novu(process.env.NOVU_API_KEY!);
 
-export const echo = new Echo({
-	apiKey: process.env.NOVU_API_KEY!,
-	devModeBypassAuthentication: process.env.NODE_ENV === "development",
-});
-
 export async function POST(req: NextRequest) {
-    const { subject, message, subscribers } = await req.json();
+	const { subject, message, subscribers } = await req.json();
 
-    let status = true
 	subscribers.forEach(async (subscriber: SubscribersData) => {
 		const { data } = await novu.trigger("newsletter", {
 			to: {
@@ -26,23 +22,15 @@ export async function POST(req: NextRequest) {
 				message,
 				firstName: subscriber.firstName,
 			},
-        });
-        if (!data.data.transactionId) {
-            status = false
-        }
-    });
+		});
+		console.log(data);
+	});
 
-    if (status) {
-			return NextResponse.json(
-                {
-                    message: "Emails sent successfully", success: true,
-                },
-				{ status: 200 }
-			);
-		} else {
-			return NextResponse.json(
-				{ message: "Emails not sent", success: false },
-				{ status: 500 }
-			);
-		}
+	return NextResponse.json(
+		{
+			message: "Emails sent successfully",
+			success: true,
+		},
+		{ status: 200 }
+	);
 }
